265591073a37651c7a399fe733bf09ceafad5ec3
EcolesPeretz <ecoles.pub@gmail.com>
2026-09-15 09:19:25 +0000

    chore: fix encoding in tsx files

--- prisma/seed-admin.ts
+++ prisma/seed-admin.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from '@prisma/client'
-import { hashPassword } from '../src/lib/auth'
+import { hashPassword } from '../src/lib/tokens'
 
 const prisma = new PrismaClient()
 
